1a0681fd2cd829c72c8b1b2d258ea694cd8e75f8
PARINGAUX Guillaume <gparingaux@gmail.com>
2015-09-27 01:59:04 +0200

    feature (Player) : FPS Camera Controller

--- Assets/Scripts/Camera/CameraMouseAim.cs
+++ Assets/Scripts/Camera/CameraMouseAim.cs
@@ -7,6 +7,9 @@ using System.Collections;
 /// <remarks>
 /// In our case, the target is a little above characters's shoulder
 /// </remarks>
+
+
+
 public class CameraMouseAim : MonoBehaviour {
 
     public const float detectionRadius = 0.15f;
@@ -23,46 +26,56 @@ public class CameraMouseAim : MonoBehaviour {
     Vector3 desiredPosition;
     Vector3 direction;
 
+	[SerializeField] private MouseLook m_MouseLook;
+	private Camera m_Camera;
+
+	public void Start ()
+	{
+		m_Camera = Camera.main;
+	}
+
     public void RunUpdate(float delta)
     {
-        if (target == null)
-        {
-            this.enabled = false;
-            return;
-        }
+		m_MouseLook.LookRotation (transform, m_Camera.transform);
 
-	    //Get mouse position
-        float horizontal = Input.GetAxis("Mouse X") * rotateSpeedH;
-        float vertical = - Input.GetAxis("Mouse Y") * rotateSpeedV;
-
-        //Calculate new desired rotation
-        Vector3 desiredRotationEuler = desiredRotation.eulerAngles;
-        desiredRotationEuler.x = desiredRotationEuler.x + vertical;
-        desiredRotationEuler.y = desiredRotationEuler.y + horizontal;
-        //Wrap value to be in -180/+180 range to enable clamping
-        if (desiredRotationEuler.x > 180) { desiredRotationEuler.x = -(360 - desiredRotationEuler.x); }
-        desiredRotationEuler.x = Mathf.Clamp(desiredRotationEuler.x, -clampV, clampV);
-        desiredRotation = Quaternion.Euler(desiredRotationEuler);
-
-        // Calculate a new desired position from rotation     
-        desiredPosition = target.transform.position - (desiredRotation * offset);
-
-        // Raycast from target to desired cam position
-        direction = (desiredPosition - target.transform.position).normalized;
-        float distance = Vector3.Distance(desiredPosition, target.transform.position);
-        if (Physics.SphereCast(target.transform.position, detectionRadius, direction, out hit, distance))
-        {
-            //Place the camera just before the obstacle
-            //0.5 here ensure we are just before the obstacle
-            transform.position = hit.point + 0.4f * -direction;
-        }
-        else
-        {
-            //place the camera at the desired spot
-            transform.position = desiredPosition;
-        }
-
-        //Always look at player
-        transform.LookAt(target.transform);
+//        if (target == null)
+//        {
+//            this.enabled = false;
+//            return;
+//        }
+//
+//	    //Get mouse position
+//        float horizontal = Input.GetAxis("Mouse X") * rotateSpeedH;
+//        float vertical = - Input.GetAxis("Mouse Y") * rotateSpeedV;
+//
+//        //Calculate new desired rotation
+//        Vector3 desiredRotationEuler = desiredRotation.eulerAngles;
+//        desiredRotationEuler.x = desiredRotationEuler.x + vertical;
+//        desiredRotationEuler.y = desiredRotationEuler.y + horizontal;
+//        //Wrap value to be in -180/+180 range to enable clamping
+//        if (desiredRotationEuler.x > 180) { desiredRotationEuler.x = -(360 - desiredRotationEuler.x); }
+//        desiredRotationEuler.x = Mathf.Clamp(desiredRotationEuler.x, -clampV, clampV);
+//        desiredRotation = Quaternion.Euler(desiredRotationEuler);
+//
+//        // Calculate a new desired position from rotation     
+//        desiredPosition = target.transform.position - (desiredRotation * offset);
+//
+//        // Raycast from target to desired cam position
+//        direction = (desiredPosition - target.transform.position).normalized;
+//        float distance = Vector3.Distance(desiredPosition, target.transform.position);
+//        if (Physics.SphereCast(target.transform.position, detectionRadius, direction, out hit, distance))
+//        {
+//            //Place the camera just before the obstacle
+//            //0.5 here ensure we are just before the obstacle
+//            transform.position = hit.point + 0.4f * -direction;
+//        }
+//        else
+//        {
+//            //place the camera at the desired spot
+//            transform.position = desiredPosition;
+//        }
+//
+//        //Always look at player
+//        transform.LookAt(target.transform);
 	}
 }
