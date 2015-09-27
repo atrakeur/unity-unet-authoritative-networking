fe8045817dd7213a11bf093f31f81bae612112b4
atrakeur <atrakeur@gmail.com>
2015-09-27 13:25:54 +0200

    feature (Map): Added labo map

---
+++ Assets/Scripts/SuperCharacterController/RecursiveBspTree.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecursiveBspTree : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.AddBSPTreeToChilds(this.transform);
+	}
+
+    void AddBSPTreeToChilds(Transform transform)
+    {
+        //Add BSPTree component if there is a mesh collider AND there is no BSPTree script allready attached
+        if (transform.GetComponent<MeshCollider>() != null) {
+            if (transform.GetComponent<BSPTree>() == null)
+            {
+                transform.gameObject.AddComponent<BSPTree>();
+            }
+        }
+
+        //Loop all childs and call the same method
+        foreach (Transform child in transform)
+        {
+            AddBSPTreeToChilds(child);
+        }
+    }
+
+}
