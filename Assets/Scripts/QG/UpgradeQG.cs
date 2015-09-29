853b92267e715253419edb30b53de324761a06ec
PARINGAUX Guillaume <gparingaux@gmail.com>
2015-09-29 14:37:35 +0200

    QG (features) : In progress

---
+++ Assets/Scripts/QG/UpgradeQG.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UpgradeQG : MonoBehaviour {
+	public string myName;
+	public List<GameObject> prerequisites;
+	public List<Cost> costs;
+}
